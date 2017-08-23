a97655732f6c949e3362149c8f2b7f57f74afd50
tedc <egrandinetti@gmail.com>
2017-08-23 10:30:24 +0200

    flex slider splitted

--- partials/flex-slider.php
+++ partials/flex-slider.php
@@ -3,9 +3,10 @@
     		$images = get_sub_field('flex_images'); 
     		$dida = get_sub_field('flex_dida');
     		if($images) : ?>
-<div class="flex__slider">
+<div class="flex__slider<?php echo (get_sub_field('columns')) ? ' flex__slider--split' : ''; ?>">
     <div class="shift__slider">
     	<?php foreach ($images as $image) : ?>
+        <?php if(!get_sub_field('columns')) : ?>
     	<figure class="shift__slide" style="background-image:url(<?php echo $image['url']; ?>)">
     		<?php echo wp_get_attachment_image( $image['ID'], 'large' ); ?>
     		<?php if($dida) : ?>
@@ -14,6 +15,15 @@
     		</figcaption>
     		<?php endif; ?>
     	</figure>
+    <?php else : ?>
+        <div class="shift__slide shift__slide--columns">
+            <div class="slide__image"  style="background-image:url(<?php echo $image['url']; ?>)">
+                <?php echo wp_get_attachment_image( $image['ID'], 'large' ); ?>
+            </div>
+            <div class="slide__dida">
+                <?php echo $image['description']; ?>
+            </div>
+        </div>
     	<?php endforeach; ?>
     </div>
 </div>
