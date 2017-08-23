a222c8ea603cf8f5e1046effc7f52cb2ea865378
tedc <egrandinetti@gmail.com>
2017-08-23 10:38:43 +0200

    padding

--- partials/flex-paragraph.php
+++ partials/flex-paragraph.php
@@ -1,6 +1,17 @@
 <?php if ( get_row_layout() == 'flex_paragraph' ) : ?>
-<?php if( get_sub_field('flex_paragraph') ) : ?>
-<div class="paragrafo">
+<?php if( get_sub_field('flex_paragraph') ) : 
+$padding = '';
+if(get_sub_field('padding') == 1) {
+	$padding = ' paragrafo--padding-top';
+} elseif(get_sub_field('padding') == 2) {
+	$padding = ' paragrafo--padding-bottom';
+} elseif(get_sub_field('padding') == 3) {
+	$padding = ' paragrafo--padding-no';
+} else {
+	$padding = '';
+}
+?>
+<div class="paragrafo<?php echo $padding; ?>">
   <?php 
   $sm = 'data-scrollmagic=\'{"tween":[{"y":50,"opacity":0},{"y":0,"opacity":1}],"duration":0,"triggerHook":0.8,"offset":100}\'';
   $p = get_sub_field('flex_paragraph');
