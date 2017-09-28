b105a7a4190a108bcbf30ef3ab6dffaaa3a6ac4a
tedc <egrandinetti@gmail.com>
2017-09-28 09:45:35 +0200

    gnata

--- partials/flex-header.php
+++ partials/flex-header.php
@@ -1,6 +1,6 @@
 <?php if ( get_row_layout() == 'flex_page_header' ) : ?>
     <?php $pageHeader = get_sub_field('page_header'); ?>
-    <div class="page-header masked-row" data-scrollmagic='{"class":"unmasked-row","triggerHook":0.75,"duration":0}' style="background-image: url('<?php echo $pageHeader; ?>');">
+    <div class="page-header masked-row" data-scrollmagic='{"class":"unmasked-row","triggerHook":0.75,"duration":0,"reverse":false}' style="background-image: url('<?php echo $pageHeader; ?>');">
 
     </div>
 <?php endif; ?>
