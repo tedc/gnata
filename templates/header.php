df4c707502899a3bb79bb45f22b965f0c0cd1fe4
tedc <egrandinetti@gmail.com>
2017-10-25 15:34:58 +0200

    wrapper

--- templates/header.php
+++ templates/header.php
@@ -25,7 +25,7 @@
     
     <header class="header header--home">
       <div class="header__container">
-        <div class="header__logo-container">
+        <div class="header__logo-wrapper">
           <div class="header__logo">
             <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/gnata-logo.svg" class="header__logo-home svg-inject" alt=""></a>
           </div>
