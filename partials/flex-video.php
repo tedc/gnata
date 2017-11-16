25e76d79866e61e9f0b8383de78210308e8598d4
tedc <egrandinetti@gmail.com>
2017-11-16 11:37:44 +0100

    (src=\")(.*)(\")

--- partials/flex-video.php
+++ partials/flex-video.php
@@ -2,7 +2,7 @@
 <div class="flex__video">
 	<?php 
 	$iframe = get_sub_field('flex_video');
-	$iframe = preg_replace('/(src=\"[^"]*\")/g', '$1&rel=0&showinfo=0', $iframe);
+	$iframe = preg_replace('/(src=\")(.*)(\")/g', '$2&rel=0&showinfo=0', $iframe);
 	echo $iframe;?>
 </div>
 <?php endif; ?>
