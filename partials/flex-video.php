4e95ab4a9d3663bbed00d990b6840f934e34ce91
tedc <egrandinetti@gmail.com>
2017-11-16 11:34:11 +0100

    send new video

--- partials/flex-video.php
+++ partials/flex-video.php
@@ -1,5 +1,8 @@
 <?php if(get_sub_field('flex_video')) : ?>
 <div class="flex__video">
-	<?php the_sub_field('flex_video'); ?>
+	<?php 
+	$iframe = get_sub_field('flex_video');
+	$iframe = preg_replace('/(src=\"[^"]*\")/g', '$1&rel=0&showinfo=0', $iframe);
+	echo $iframe;?>
 </div>
 <?php endif; ?>
