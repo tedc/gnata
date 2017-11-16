96bc7d07c6f7ced2082dd907f5b4967434d1d774
tedc <egrandinetti@gmail.com>
2017-11-16 11:51:15 +0100

    remove controls

--- partials/flex-video.php
+++ partials/flex-video.php
@@ -2,7 +2,7 @@
 <div class="flex__video">
 	<?php 
 	$iframe = get_sub_field('flex_video');
-	$iframe = str_replace('oembed', 'oembed&rel=0&showinfo=0', $iframe);
+	$iframe = str_replace('oembed', 'oembed&rel=0&showinfo=0&controls=0', $iframe);
 	echo $iframe;?>
 </div>
 <?php endif; ?>
