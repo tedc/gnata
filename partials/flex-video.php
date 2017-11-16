6320ecb70c08d95b6de34864e96eb4189ac0460c
tedc <egrandinetti@gmail.com>
2017-11-16 11:44:37 +0100

    oembed

--- partials/flex-video.php
+++ partials/flex-video.php
@@ -2,7 +2,7 @@
 <div class="flex__video">
 	<?php 
 	$iframe = get_sub_field('flex_video');
-	//$iframe = str_replace('?oembed', replace, subject)('/(src=\")(.*)(\")/g', '$1$2&rel=0&showinfo=0$3', $iframe);
+	$iframe = str_replace('oembed', 'oembed&rel=0&showinfo=0', $iframe);
 	echo $iframe;?>
 </div>
 <?php endif; ?>
